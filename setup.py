be6c1bd7d0592f1aa3f2804a0f1bd3b5e3448949
Parsifal1916 <120274850+Parsifal1916@users.noreply.github.com>
2023-08-05 20:39:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="nBodyEngine",
+    version="1.0.0",
+    author="Federico B.",
+    description="A lightweight engine to simulate the n-Body problem with Newtonian and post-Newtonian mechanics",
+    packages=find_packages(),
+    install_requires=[
+        "numpy", "matplotlib"
+    ],
+)
