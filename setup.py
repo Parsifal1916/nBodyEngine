23219e9c6225a3af1a32749e6d7116bbbfce89e7
Parsifal1916 <120274850+Parsifal1916@users.noreply.github.com>
2023-08-06 13:34:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="1.0.0",
     author="Federico B.",
     description="A lightweight engine to simulate the n-Body problem with Newtonian and post-Newtonian mechanics",
+    long_description='A lightweight engine to simulate the n-Body problem with Newtonian and post-Newtonian mechanics. It can now simulate both in 2 dimensions and 3. All the simulations can be run from a json file to facilitate simulation data storage. For further documentation see https://github.com/Parsifal1916/nBodyEngine/',
     packages=find_packages(),
     install_requires=[
         "numpy", "matplotlib"
