568e148311b0ce43443bb62e236b78ea547b57f0
Parsifal1916 <120274850+Parsifal1916@users.noreply.github.com>
2023-08-17 11:04:18 +0200

    Update __init__.py

--- nBodyEngine/__init__.py
+++ nBodyEngine/__init__.py
@@ -5,35 +5,37 @@ import os
 
 print('nBodyEngine Installed Successfully!')
 
+
 def simulateFromJson(jsonFile: str) -> None:
 	print('loading json file...')
 	with open(jsonFile, 'r') as file:
 		content = json.load(file) # estrae il contenuto del file
 
-		#crea i corpi
-		bodies: list = []
-		_bodies: list = content['Bodies']
-		for i in _bodies:
-			try: useAccurateSize = content['Simulation']['useAccurateSize']
-			except KeyError: useAccurateSize = False
-			bodies.append(Body.Body(
-				i, 
-				bodies, 
-				useAccurateSize = content['Simulation'].get('useAccurateSize', False),
-				includeSR = content['Simulation'].get('includeSR', False),
-				dimensions = content['Simulation'].get('style', 2)
-				))
-
-		for i in bodies: i.bodies = bodies
-
-
-		#comincia la simulazione
 		simulation=GraphEngine.Graph(
-			bodies, 
+			[], 
 			content['Simulation']['size'], 
 			content['Simulation']['graphs'], 
 			toggleInstableOrbits = content['Simulation'].get('toggleInstableOrbits', False),
 			dimensions = content['Simulation'].get('style', 2)
 		)
+
+		bodies: list = []
+		_bodies: list = content['Bodies']
+
+		#crea i corpi basandosi sul file json
+		for i in _bodies:
+			try: useAccurateSize = content['Simulation']['useAccurateSize']
+			except KeyError: useAccurateSize = False
+			bodies.append(Body.Body(
+				i, 
+				simulation, 
+				useAccurateSize = content['Simulation'].get('useAccurateSize', False),
+				includeSR = content['Simulation'].get('includeSR', False),
+				dimensions = content['Simulation'].get('style', 2)
+				))
+
+		simulation.bodies = bodies
+
+		#comincia la simulazione
 		print('starting the simulation..')
 		simulation.start(content['Simulation']['speed'])
