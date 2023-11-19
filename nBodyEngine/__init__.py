71c259ba3c4e15df6ae43c1a6b12191501d479cb
Parsifal1916 <120274850+Parsifal1916@users.noreply.github.com>
2023-11-19 14:34:14 +0100

    Update __init__.py

--- nBodyEngine/__init__.py
+++ nBodyEngine/__init__.py
@@ -10,6 +10,15 @@ def simulateFromJson(jsonFile: str) -> None:
 	with open(jsonFile, 'r') as file:
 		content = json.load(file) # estrae il contenuto del file
 
+		#comincia la simulazione
+		simulation=GraphEngine.Graph(
+			content['Simulation']['size'], 
+			graphs = content['Simulation']['graphs'], 
+			toggleInstableOrbits = content['Simulation'].get('toggleInstableOrbits', False),
+			pathTracing = content['Simulation'].get('pathTracing', False),
+			dimensions = content['Simulation'].get('style', 2)
+		)
+
 		#crea i corpi
 		bodies: list = []
 		_bodies: list = content['Bodies']
@@ -17,24 +26,15 @@ def simulateFromJson(jsonFile: str) -> None:
 			try: useAccurateSize = content['Simulation']['useAccurateSize']
 			except KeyError: useAccurateSize = False
 			bodies.append(Body.Body(
+				simulation,
 				i, 
-				bodies, 
 				useAccurateSize = content['Simulation'].get('useAccurateSize', False),
 				includeSR = content['Simulation'].get('includeSR', False),
 				dimensions = content['Simulation'].get('style', 2)
 				))
 
-		for i in bodies: i.bodies = bodies
-		print(content['Simulation'].get('pathTracing', False))
+		del _bodies
+		simulation.bodies = bodies
 
-		#comincia la simulazione
-		simulation=GraphEngine.Graph(
-			bodies, 
-			content['Simulation']['size'], 
-			content['Simulation']['graphs'], 
-			toggleInstableOrbits = content['Simulation'].get('toggleInstableOrbits', False),
-			pathTracing = content['Simulation'].get('pathTracing', False),
-			dimensions = content['Simulation'].get('style', 2)
-		)
 		print('starting the simulation..')
 		simulation.start(content['Simulation']['speed'])
